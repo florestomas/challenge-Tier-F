ea6e78b1aeedf09a49004c3048c776f39ed3d06e
florestomas <floresmancillatomas@gmail.com>
2025-01-16 11:11:19 -0300

    ahi va - fianl commit VERSION 1.0

--- script.js
+++ script.js
@@ -22,42 +22,58 @@ let productos = [
   },
 ];
 
-// Alta de producto
-document
-  .getElementById("formProducto")
-  .addEventListener("submit", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+  const formProducto = document.getElementById("formProducto");
+
+  formProducto.addEventListener("submit", function (event) {
+    // Evita que la página se recargue
     event.preventDefault();
 
+    // Captura los datos del formulario
     const nuevoProducto = {
       titulo: document.getElementById("titulo").value,
       descripcion: document.getElementById("descripcion").value,
       precio: parseFloat(document.getElementById("precio").value),
       ubicacion: document.getElementById("ubicacion").value,
-      imagen: "images/dragodevs_logo.jpeg", // Imagen predeterminada (puedes agregar un campo para cargar imágenes)
     };
 
+    console.log(nuevoProducto);
+    // Agrega el nuevo producto al array
     productos.push(nuevoProducto);
+
+    // Recarga las cards
+    renderizarProductos();
+
+    // Cierra el modal
+    $("#formModal").modal("hide");
+
+    // Limpia el formulario
+    formProducto.reset();
   });
+});
 
 // Función para renderizar las tarjetas
 function renderizarProductos() {
   const contenedor = document.querySelector(".album .container .row");
+
+  // Limpia el contenido previo del contenedor
+  contenedor.innerHTML = "";
   productos.forEach((producto) => {
     const card = document.createElement("div");
     card.classList.add("col-md-4");
     card.innerHTML = `
-        <div class="card mb-4">
-          <img src="${producto.imagen}" class="card-img-top" alt="${producto.titulo}" />
-          <div class="card-body">
-            <h5 class="card-title">${producto.titulo}</h5>
-            <p class="card-text">${producto.descripcion}</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <a href="#" class="btn btn-primary">Añadir al carrito</a>
-              <span>Precio: $${producto.precio} USD</span>
+          <div class="card mb-4">
+            <img src="${producto.imagen}" class="card-img-top" alt="${producto.titulo}" />
+            <div class="card-body">
+              <h5 class="card-title">${producto.titulo}</h5>
+              <p class="card-text">${producto.descripcion}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                <span>Precio: $${producto.precio} USD</span>
+              </div>
             </div>
           </div>
-        </div>
-      `;
+        `;
     contenedor.appendChild(card);
   });
 }
